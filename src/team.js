884c960a1f8846f0afafb65fca1ba80d4a3d137c
manoel <manoel.josefneto@gmail.com>
2018-03-16 12:24:09 -0300

    Mudanças de Fevereiro

--- src/team.js
+++ src/team.js
@@ -1,27 +1,21 @@
 import artur from './assets/imgs/artur.jpeg'
-import bia from './assets/imgs/bia.jpeg'
-import lucasp from './assets/imgs/lucasp.jpeg'
 import manoel from './assets/imgs/maneco.jpg'
 import math from './assets/imgs/math.jpeg'
 import rodolfo from './assets/imgs/rodo.jpeg'
 import pablo from './assets/imgs/pablo.jpeg'
+import jp from './assets/imgs/JP.jpg'
 import pelu from './assets/imgs/pelu.jpeg'
 import veni from './assets/imgs/veni.jpeg'
-import victor from './assets/imgs/victor.jpeg'
-import lucast from './assets/imgs/lucast.jpg'
 import { replaceImgs } from "./utils";
 
 const imgs = {
     artur,
-    bia,
-    lucasp,
     manoel,
     math,
     rodolfo,
     pablo,
     pelu,
-    lucast,
     veni,
-    victor
+    jp,
 }
 replaceImgs('img[id^=team]', imgs)
